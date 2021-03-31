af53e6af1ba87bda33bf8dc564d78b0ffdb0cd48
Rishabh Rathore <77366500+rishabhrathore055@users.noreply.github.com>
2021-03-31 16:22:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# Using Python sorted() funtion to sort a list:
+# Using Python sorted() funtion to sorting a list:
 # The sorted() function returns a sorted list of the specified iterable object.
-# You can specify ascending or descending order. Strings are sorted alphabetically, and numbers are sorted numerically. 
-# You cannot sort a list that contains both string values and numeric values.
+# You can specify ascending or descending orders. Strings are sorted alphabetically, and numbers are sorted numerically. 
+# You cannot sort a list that contains both string values as well as numeric values.
 
-# Sort numeric:
+# Sorting numeric values:
 a = (87,23,3,87,99,54,5,10,8,28)
 x = sorted(a)
 print(x)
 
-# Sort strings ascending order:
+# Sorting strings ascending order:
 a = ("m", "k", "a", "c", "f", "o", "e", "v")
 x = sorted(a)
 print(x)
 
-# Sort strings descending order:
+# Sorting strings descending order:
 a =  ("m", "k", "a", "c", "f", "o", "e", "v")
 x = sorted(a, reverse=True)
 print(x)
